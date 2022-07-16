9730b695d0c2210d4f35ebe8467905dabef3947e
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-07-16 16:09:33 +0900

    apply prettier

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, type DocumentContext } from "next/document"
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  type DocumentContext,
+} from "next/document"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
