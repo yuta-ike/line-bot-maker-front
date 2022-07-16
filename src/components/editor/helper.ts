c7b00800fed9607ab904ac057956dbb5f4dd94fe
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-07-16 16:08:14 +0900

    add prettier

--- src/components/editor/helper.ts
+++ src/components/editor/helper.ts
@@ -1,13 +1,13 @@
-import { Edge, GraphNode } from "./node";
+import { Edge, GraphNode } from "./node"
 
 export const getPointPos = (
   inOrOut: "in" | "out",
   node: GraphNode,
-  pointId: number
+  pointId: number,
 ) => ({
   x: node.pos.x + (160 / (node[`${inOrOut}Points`].length + 1)) * (pointId + 1),
   y: node.pos.y + (inOrOut === "in" ? 0 : 80),
-});
+})
 
 export const isSameEdge = (e1: Edge, e2: Edge) => {
   return (
@@ -15,5 +15,5 @@ export const isSameEdge = (e1: Edge, e2: Edge) => {
     e1.start.pointId === e2.start.pointId &&
     e1.end.nodeId === e2.end.nodeId &&
     e1.end.pointId === e2.end.pointId
-  );
-};
+  )
+}
