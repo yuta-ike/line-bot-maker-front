0bb55a9f18cf32c2356afde6beb40cbde26191a5
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-07-23 00:50:15 +0900

    back button dashboard

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link"
+import { MdHome } from "react-icons/md"
+
 export type TopBarProps = {
   name: string
   onChangeName: (name: string) => void
@@ -7,7 +10,12 @@ const TopBar: React.FC<TopBarProps> = ({ name, onChangeName }) => {
   return (
     <>
       <div className="z-30 flex items-center bg-red-300 p-2 pl-4">
-        <div>PJ名：</div>
+        <Link href="/">
+          <a className="rounded-full text-gray-800 transition hover:text-gray-800/50">
+            <MdHome size="24px" />
+          </a>
+        </Link>
+        <div className="ml-4">PJ名：</div>
         <input
           className="rounded px-2 py-1"
           placeholder="プロジェクト名"
