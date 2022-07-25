e3cb48f453b17fc949139c5c3e81f82389b7f684
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-07-25 21:14:00 +0900

    apply formatter

--- src/services/api_service.ts
+++ src/services/api_service.ts
@@ -1,64 +1,74 @@
-import useSWR from 'swr'
+import useSWR from "swr"
 import axios from "axios"
 
-const url = 'http://localhost:8000'
+const url = "http://localhost:8000"
 
-const fetcher = (url:string) => axios.get(url).then(res => res.data)
+const fetcher = (url: string) => axios.get(url).then((res) => res.data)
 
-export function GetBots () {
-    const { data, error } = useSWR(url+'/getIdToken/bot', fetcher)
+export function GetBots() {
+  const { data, error } = useSWR(url + "/getIdToken/bot", fetcher)
 
-    return {
-        user: data,
-        isLoading: !error && !data,
-        isError: error
-    }
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  }
 }
 
-export function GetBot (id: string) {
-    const { data, error } = useSWR(url+`/getIdToken/bot/${id}`, fetcher)
+export function GetBot(id: string) {
+  const { data, error } = useSWR(url + `/getIdToken/bot/${id}`, fetcher)
 
-    return {
-        user: data,
-        isLoading: !error && !data,
-        isError: error
-    }
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  }
 }
 
-export function CreateBot (bot_id: string, name: string, developerId: string, flowChart: string) {
-    const res = axios.post(url+'/getIdToken/bot', {
-        bot_id: bot_id,
-        name: name,
-        developerId: developerId,
-        flowChart: flowChart
-      })
+export function CreateBot(
+  bot_id: string,
+  name: string,
+  developerId: string,
+  flowChart: string,
+) {
+  const res = axios.post(url + "/getIdToken/bot", {
+    bot_id: bot_id,
+    name: name,
+    developerId: developerId,
+    flowChart: flowChart,
+  })
 
-    return(res)
+  return res
 }
 
-export function UpdateBot (bot_id: string, name: string, developerId: string, flowChart: string) {
-    let data: {[key: string]: string};
+export function UpdateBot(
+  bot_id: string,
+  name: string,
+  developerId: string,
+  flowChart: string,
+) {
+  let data: { [key: string]: string }
 
-    data = {}
-    if (name){
-        data['name'] = name
-    }
+  data = {}
+  if (name) {
+    data["name"] = name
+  }
 
-    if (developerId){
-        data['developerId'] = developerId
-    }
+  if (developerId) {
+    data["developerId"] = developerId
+  }
 
-    if (flowChart){
-        data['flowChart'] = flowChart
-    }
+  if (flowChart) {
+    data["flowChart"] = flowChart
+  }
 
-    const res = axios.put(url+`/getIdToken/bot/${bot_id}`, data)
+  const res = axios.put(url + `/getIdToken/bot/${bot_id}`, data)
 
-    return(res)
+  return res
 }
 
-export function DeleteBot (bot_id: string) {
-    const res = axios.delete(url+`/getIdToken/bot/${bot_id}`)
+export function DeleteBot(bot_id: string) {
+  const res = axios.delete(url + `/getIdToken/bot/${bot_id}`)
 
-    return res
+  return res
 }
