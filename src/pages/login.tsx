e3e14213d422d13e07300539ce041b1d7a528928
nt <kumypc@gmail.com>
2022-09-16 20:57:23 +0900

    Lineログイン画面の作成

---
+++ src/pages/login.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { useLiffOperation, useUser } from "../provider/LiffProvider"
+
+const Login = () => {
+  const { logout, login } = useLiffOperation()
+  const router = useRouter()
+  const user = useUser()
+
+  //ログインしている場合のリダイレクト処理を書いたつもりだが，ログイン画面がローカル，ダッシュボードが本番環境のため正しく動くかどうかは現在不明
+  useEffect(() => {
+    user != null && router.push("/")
+  })
+
+  return (
+    <>
+      <Head>
+        <title>LINE Bot Maker</title>
+      </Head>
+      <div className="h-screen bg-gray-800">
+        <div className="flex h-full items-center justify-center bg-gray-100">
+          <div className=" flex h-[50%] w-[50%] flex-col items-center justify-evenly rounded-lg border bg-white shadow">
+            <div className="text-4xl font-extrabold">ふろちゃでぼっと</div>
+            <button
+              onClick={() => login()}
+              className="w-[50%] rounded-full border-2 border-green-500 bg-green-500 px-8 py-4 text-3xl font-extrabold text-white"
+            >
+              LINEでログイン
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
